88bab5ce0e6f0bd630ed5bd8065e293487e9b922
Hendry <hendry.wijaya@gmail.com>
2023-01-17 11:54:31 +0700

    Add more example to js-lesson-7.

--- js-lessons/js-07-Conditionals/js/main.js
+++ js-lessons/js-07-Conditionals/js/main.js
@@ -1,28 +1,71 @@
-// Math Methods
-
-console.log(Math.PI);
-console.log(Math.trunc(Math.PI));
-console.log(Math.round(Math.PI));
-
-console.log(Math.ceil(Math.PI));
-console.log(Math.floor(Math.PI));
-
-console.log(Math.pow(5,2));
-console.log(Math.min(2,0.5,9));
-console.log(Math.max(2,0.5,9.2));
-
-console.log(Math.random());
-console.log(Math.random());
-
-
-// use the floor instead of the ceil function to generate random number 
-console.log(Math.floor(Math.random()*10)+1);
-console.log(Math.floor(Math.random()*10)+1);
-console.log(Math.floor(Math.random()*10)+1);
-console.log(Math.floor(Math.random()*10)+1);
-console.log(Math.floor(Math.random()*10)+1);
-console.log(Math.floor(Math.random()*10)+1);
-console.log(Math.floor(Math.random()*10)+1);
+// Conditionals: IF statements
 
 
 
+let customerIsBanned = 0;
+let soup = "beef noodle soup";
+let crackers = 0;
+let reply;
+
+
+
+if (customerIsBanned) {
+    reply = "No soup for you!";
+} else if (soup && crackers) {
+    reply = `Here's your order of ${soup} & crakers.`;
+} else if (soup) {
+    reply = `Here's your order of ${soup}.`
+} else {
+    reply = `Sorry, we're out of ${soup}`;
+}
+
+console.log(reply);
+
+
+// Conditionals: Switch Statements
+
+switch (Math.floor(Math.random() * 5 + 1)) {
+    case 1:
+        console.log(1);
+        break;
+        
+    case 2:
+        console.log(2);
+        break;
+    case 3:
+        console.log(3);
+        break;
+    default:
+        console.log('No Match!');
+
+}
+
+let playerOne = "rock";
+let computer = "scissor";
+
+switch (playerOne) {
+    case computer:
+        console.log("Tie game!");
+        break;
+    case "rock":
+        if (computer === "paper") {
+            console.log("computer wins!");
+        } else {
+            console.log("playerOne wins");
+        }
+        break;
+    case "paper":
+        if (computer === "scissor") {
+            console.log("computer wins!");
+        } else {
+            console.log("playerOne wins");
+        }
+        break;
+    default:
+        if (computer === "rock") {
+            console.log("computer wins!");
+        } else {
+            console.log("playerOne wins");
+        }
+        break;
+}
