421fab2013d2ef59ebab849c6395439b76831cfd
Hendry <hendry.wijaya@gmail.com>
2023-01-18 22:05:21 +0700

    Add js-15-Scope exercise.

---
+++ js-lessons/js-14-Scope/js/main.js
@@ -0,0 +1,27 @@
+// Variable Scope
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(`global: ${x}`);
+console.log(`global: ${y}`);
+console.log(`global: ${z}`);
+
+function myFun() {
+    var x = 10;
+    const z = 5;
+    console.log(`function: ${x}`);
+    console.log(`function: ${y}`);
+    console.log(`function: ${z}`);
+
+    {
+        var x = 11;
+        const z = 6;
+        console.log(`block: ${x}`);
+        console.log(`block: ${y}`);
+        console.log(`block: ${z}`);
+
+    }
+}
+myFun();
