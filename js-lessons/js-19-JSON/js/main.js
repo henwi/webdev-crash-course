0f47a18cbea06930fab65f22d6b4ae30c53a91d6
Hendry <hendry.wijaya@gmail.com>
2023-01-20 19:54:02 +0700

    Add js-19-JSON exercise.

---
+++ js-lessons/js-19-JSON/js/main.js
@@ -0,0 +1,30 @@
+// JSON - Javascript Object Notation
+
+
+/* 
+JSON is used to send and receive data.
+JSON is a text format that is completely language independent.
+Meaning JSON is used to send & receive data in many language
+not just in Javascript.
+*/
+
+const myObj = {
+    name: "Dave",
+    hobbies: ["eat", "sleep", "code"],
+    hello: function () {
+        console.log("Hello!");
+    }
+};
+console.log(myObj);
+console.log(myObj.name);
+myObj.hello();
+console.log(typeof myObj);
+
+const sendJSON = JSON.stringify(myObj);
+console.log(sendJSON);
+console.log(typeof sendJSON);
+console.log(sendJSON.name);
+
+const receiveJSON = JSON.parse(sendJSON);
+console.log(receiveJSON);
+console.log(typeof receiveJSON);
