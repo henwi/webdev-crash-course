67d6f13ee5ffddb5ee1ded6dcb5b466a680ebc7c
Hendry <hendry.wijaya@gmail.com>
2023-01-24 12:02:05 +0700

    Add js-22 Event Listener exercise.

---
+++ js-lessons/js-22-Event_Listeners/js/main.js
@@ -0,0 +1,98 @@
+// Javascript Event Listeners
+
+
+// Syntax: addEventListener(event, function, useCapture)
+// === ===
+/* 
+
+const doSomething = () => {
+    alert("doing something");
+};
+
+h2.addEventListener("click", doSomething, false);
+h2.removeEventListener("click", doSomething, false);
+
+h2.addEventListener("click", (event) => {
+    console.log(event.target);
+    event.target.textContent = "Clicked";
+});
+ */
+
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        console.log("readyState: complete");
+        // Exercise 2
+        // initApp();
+
+        // Exercise 3
+        initAppForm();
+    }
+});
+
+const initApp = () => {
+
+    const view =  document.querySelector("#view2");
+    const div = view.querySelector("div");
+    const h2 = div.querySelector("h2");
+
+    view.addEventListener(
+        "click", 
+        (event) => {
+        /*  
+        view.style.backgroundColor = "purple"; 
+        view.classList.add("purple");
+        view.classList.remove("darkblue"); 
+        */
+       view.classList.toggle("purple");
+       view.classList.toggle("darkblue");
+    }, 
+    false);
+    
+    div.addEventListener(
+        "click", 
+        (event) => {
+        /* 
+        event.stopPropagation(); // stop the propagation of the event bubbling
+        div.style.backgroundColor = "blue";
+        */
+        div.classList.toggle("blue");
+        div.classList.toggle("black");
+    }, 
+    false);
+
+    h2.addEventListener(
+        "click", 
+        (event) => {
+            const myText = event.target.textContent;
+            myText === "My 2nd View"
+            ? (event.target.textContent = "Clicked")
+            : (event.target.textContent = "My 2nd View");
+    }, 
+    false);
+
+    const nav = document.querySelector("nav");
+    nav.addEventListener(
+        "mouseover", 
+        (event) => {
+            event.target.classList.add("height100");
+        });
+        nav.addEventListener(
+            "mouseout",
+            (event) => {
+                event.target.classList.remove("height100");
+            });
+};
+
+
+const initAppForm = () => {
+    const view3 = document.querySelector("#view3");
+    const myForm = view3.querySelector("#myForm");
+    myForm.addEventListener(
+        "submit",
+        (event) => {
+            event.preventDefault();
+
+            console.log("submit event");
+        });
+
+}
